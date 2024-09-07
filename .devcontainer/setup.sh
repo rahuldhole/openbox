4cb93150f4dec9862552805c42638504fc3537e9
RAHUL DHOLE <rdhole95@gmail.com>
2024-09-07 16:20:08 +0000

    install google-chrome

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -11,5 +11,14 @@ sudo DEBIAN_FRONTEND=noninteractive apt update
 sudo DEBIAN_FRONTEND=noninteractive apt install -y tilix openbox 
 sudo DEBIAN_FRONTEND=noninteractive apt install -y xrdp
 
+
+# Install Google Chrome
+mkdir ~/Downloads
+wget -P ~/Downloads https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo DEBIAN_FRONTEND=noninteractive apt install -y fonts-liberation libasound2 libnspr4 libnss3 xdg-utils libnss3 libgconf-2-4
+sudo dpkg -i ~/Downloads/google-chrome-stable_current_amd64.deb
+sudo apt --fix-broken install
+rm -rf ~/Downloads
+
 # Restart the XRDP service
 sudo service xrdp start
