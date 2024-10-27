e4914dc1a9ac01dd65aeb24348277ac5c8a7d691
rahuldhole <rdhole95@gmail.com>
2024-10-27 15:57:26 +0000

    update xrdp entrypoint

--- src/xrdp/install.sh
+++ src/xrdp/install.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+# Function to download files using curl or wget
+download() {
+  if command -v curl &> /dev/null; then
+    curl -fsSL "$1"
+  elif command -v wget &> /dev/null; then
+    wget -qO - "$1"
+  else
+    echo "Must install curl or wget to download $1" 1>&2
+    return 1
+  fi
+}
+
 sudo useradd openbox && \
     echo "openbox:openbox" | sudo chpasswd && \
     sudo usermod -aG sudo openbox
@@ -10,16 +22,24 @@ echo "xrdp xrdp/default_keyboard string us" | sudo debconf-set-selections
 # Install Openbox, XRDP, and other necessary tools
 sudo DEBIAN_FRONTEND=noninteractive apt update
 sudo DEBIAN_FRONTEND=noninteractive apt install -y kitty openbox
-sudo DEBIAN_FRONTEND=noninteractive apt install -y  dbus-x11 diodon
+sudo DEBIAN_FRONTEND=noninteractive apt install -y dbus-x11 diodon
 sudo DEBIAN_FRONTEND=noninteractive apt install -y xrdp
 
 # Openbox configuration
 mkdir -p ~/.config/openbox/
-wget -P ~/.config/openbox/ https://raw.githubusercontent.com/rahuldhole/openbox/refs/heads/main/src/xrdp/rc.xml
+download https://raw.githubusercontent.com/rahuldhole/openbox/refs/heads/main/src/xrdp/rc.xml > ~/.config/openbox/rc.xml
 
 openbox --reconfigure
 
+# Create the xrdp-entrypoint script
+sudo bash -c 'cat <<EOF > /usr/local/sbin/xrdp-entrypoint
+#!/bin/bash
 # Restart the XRDP service
 sudo service xrdp restart
-sudo echo "sudo service xrdp restart && diodon" >> /usr/local/sbin/xrdp-entrypoint
+EOF'
+
+# Make the xrdp-entrypoint script executable
 sudo chmod +x /usr/local/sbin/xrdp-entrypoint
+
+# Run the xrdp-entrypoint script
+/usr/local/sbin/xrdp-entrypoint
