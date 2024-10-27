a125578c766b6b6f6aa1b8e587a4a470309a41dc
rahuldhole <rdhole95@gmail.com>
2024-10-27 14:34:13 +0000

    add entrypoint

--- src/xrdp/install.sh
+++ src/xrdp/install.sh
@@ -20,3 +20,5 @@ openbox --reconfigure
 
 # Restart the XRDP service
 sudo service xrdp restart
+sudo echo "sudo service xrdp restart" >> /usr/local/sbin/xrdp-entrypoint
+sudo chmod +x /usr/local/sbin/xrdp-entrypoint
