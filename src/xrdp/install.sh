01a35ef9e34f3c6645b46fd2f7579348b24e6364
rahuldhole <rdhole95@gmail.com>
2024-10-27 16:16:23 +0000

    update rc.xml

--- src/xrdp/install.sh
+++ src/xrdp/install.sh
@@ -26,14 +26,30 @@ sudo DEBIAN_FRONTEND=noninteractive apt install -y dbus-x11 diodon
 sudo DEBIAN_FRONTEND=noninteractive apt install -y xrdp
 
 # Openbox configuration
-mkdir -p ~/.config/openbox/
-download https://raw.githubusercontent.com/rahuldhole/openbox/refs/heads/main/src/xrdp/rc.xml > ~/.config/openbox/rc.xml
-
-openbox --reconfigure
+sudo mkdir -p ~/.config/openbox/
 
 # Create the xrdp-entrypoint script
 sudo bash -c 'cat <<EOF > /usr/local/sbin/xrdp-entrypoint
 #!/bin/bash
+
+# Function to download files using curl or wget
+download() {
+  if command -v curl &> /dev/null; then
+    curl -fsSL "\$1"
+  elif command -v wget &> /dev/null; then
+    wget -qO - "\$1"
+  else
+    echo "Must install curl or wget to download \$1" 1>&2
+    return 1
+  fi
+}
+
+# Download rc.xml if it does not exist
+if [ ! -f ~/.config/openbox/rc.xml ]; then
+  download https://raw.githubusercontent.com/rahuldhole/openbox/refs/heads/main/src/xrdp/rc.xml > ~/.config/openbox/rc.xml
+  openbox --reconfigure
+fi
+
 # Restart the XRDP service
 sudo service xrdp restart
 EOF'
